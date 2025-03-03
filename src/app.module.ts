251110e4377d0cd5936ebad6ae293ddb1171e8d7
quan27 <npquan2704@gmail.com>
2025-03-04 06:58:15 +0700

    feat: add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import pg from 'pg';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+
+        SequelizeModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => {
+                const isDevelopment =
+                    configService.get('ENV') === 'development';
+
+                return {
+                    dialect: 'postgres',
+                    host: configService.get('DATABASE'),
+                    port: configService.get('DB_PORT'),
+                    username: configService.get('DB_USERNAME'),
+                    password: configService.get('DB_PASSWORD'),
+                    database: configService.get('DB_NAME'),
+                    dialectModule: pg,
+                    autoLoadModels: true,
+                    synchronize: true,
+                    models: [],
+                    dialectOptions: isDevelopment
+                        ? { ssl: { require: true, rejectUnauthorized: false } }
+                        : { ssl: false },
+                };
+            },
+            inject: [ConfigService],
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
