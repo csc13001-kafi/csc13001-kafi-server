6124d5f8b08b9853978650662dd58dbeaf0b1bc8
ntkwan <npquan2704@gmail.com>
2025-03-18 09:24:49 +0000

    feat: add endpoints for CRUD

---
+++ src/users/dtos/update-user.dto.ts
@@ -0,0 +1,51 @@
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import {
+    IsDateString,
+    IsNumber,
+    IsOptional,
+    IsPhoneNumber,
+    IsString,
+} from 'class-validator';
+import { CreateEmployeeDto } from './create-user.dto';
+
+export class UpdateEmployeeDto extends PartialType(CreateEmployeeDto) {
+    @ApiProperty({ required: false })
+    @IsOptional()
+    @IsString()
+    username?: string;
+
+    @ApiProperty({ required: false })
+    @IsOptional()
+    @IsString()
+    email?: string;
+
+    @ApiProperty({ required: false })
+    @IsOptional()
+    @IsPhoneNumber()
+    phone?: string;
+
+    @ApiProperty({ required: false })
+    @IsOptional()
+    @IsString()
+    address?: string;
+
+    @ApiProperty({ required: false })
+    @IsOptional()
+    @IsDateString()
+    birthdate?: string;
+
+    @ApiProperty({ required: false })
+    @IsOptional()
+    @IsNumber()
+    salary?: number;
+
+    @ApiProperty({ required: false })
+    @IsOptional()
+    @IsString()
+    workStart?: string;
+
+    @ApiProperty({ required: false })
+    @IsOptional()
+    @IsString()
+    workEnd?: string;
+}
