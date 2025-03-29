388a9c92d6276776fd79cbd6edcef7c14e57bc94
Nguyen Trung Quan <npquan2704@gmail.com>
2025-03-29 09:09:16 +0700

    Merge pull request #10 from csc13001-kafi/KAFI-3/KAFI-31
    * feat: add redis for handling refresh token
    
    * fix: invalidate old refresh token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,7 +9,7 @@ import { PassportModule } from '@nestjs/passport';
 import { AccessTokenStrategy } from './strategies/at.strategy';
 import { RefreshTokenStrategy } from './strategies/rt.strategy';
 import { AccessControlService } from '../ac/ac.service';
-
+import { RedisModule, RedisModuleOptions } from '@nestjs-modules/ioredis';
 @Module({
     imports: [
         UsersModule,
@@ -23,6 +23,27 @@ import { AccessControlService } from '../ac/ac.service';
                 },
             }),
             inject: [ConfigService],
+        }),
+        RedisModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService): RedisModuleOptions => ({
+                type: 'single',
+                options: {
+                    host: configService.get<string>('REDIS_HOST'),
+                    port: Number(configService.get<string>('REDIS_PORT')),
+                    username: configService.get<string>('REDIS_USER'),
+                    password: configService.get<string>('REDIS_PASSWORD'),
+                    tls: {
+                        rejectUnauthorized: true,
+                    },
+                    retryStrategy: (times: number) => {
+                        const delay = Math.min(times * 50, 2000);
+                        return delay;
+                    },
+                    maxRetriesPerRequest: 5,
+                },
+            }),
         }),
     ],
     controllers: [AuthController],
